1e719a495f356e0967a2b69609da69ee575154a7
Paul Yi <29391424+paulcyi@users.noreply.github.com>
2025-02-20 01:10:14 -0500

    Move main.tf and variables.tf into terraform directory

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+# main.tf
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  required_version = ">= 1.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
